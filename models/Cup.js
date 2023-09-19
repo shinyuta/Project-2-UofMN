22169ac16d9780b6b887575bc1ede7b2404087a3
PsychoRSS <Bobgonzalez516@gmail.com>
2023-09-18 20:03:04 -0600

    started working in the main branch oopsie

---
+++ models/Cup.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Cup extends Model {}
+
+Cup.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    size: {
+        type: DataTypes.INTEGER,
+        allowNull:false,
+    },
+    image: {
+        type: DataTypes.BLOB,
+        allowNull: true,
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
