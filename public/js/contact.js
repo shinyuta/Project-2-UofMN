c20d59b5331c16143764700795baaa74aa2e748d
Gabe <gabriel.velasquez0116@gmail.com>
2023-09-20 17:22:54 -0700

    made contact js to help us move information to the front end to backend. Need to work on server data

---
+++ public/js/contact.js
@@ -0,0 +1,37 @@
+const contactForm = document.querySelector('.contact-form');
+let name = document.getElementById('name');
+let subject = document.getElementById('subject');
+let email = document.getElementById('email');
+let message = document.getElementById('element');
+
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let form = {
+        name: name.value,
+        email: email.value,
+        subject: subject.value,
+        message: message.value
+    }
+    fetch('/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(form)
+    })
+    .then(response => response.text())
+    .then(data => {
+        if (data == 'success') {
+            name.value = '';
+            email.value = '';
+            subject.value = '';
+            message.value = '';
+        } else {
+
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+})
