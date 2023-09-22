d2ddaffaa195a4e8e8b4b0ed0a44c9acc21cd5fc
PsychoRSS <Bobgonzalez516@gmail.com>
2023-09-21 18:34:12 -0600

    fixed a small bugs

--- models/Cup.js
+++ models/Cup.js
@@ -32,7 +32,7 @@ Cup.init(
       allowNull: false,
     },
     launch_id: {
-      type: DataTypes.STRING,
+      type: DataTypes.INTEGER,
       references: {
         model: 'launches',
         key: 'id',
