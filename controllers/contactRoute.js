0ef3ea790b1adfe2c0b232e299e0c1822158586d
Gabe <gabriel.velasquez0116@gmail.com>
2023-09-22 19:53:24 -0700

    more housecleaning of the contact route and to render the correct things if logged in

--- controllers/contactRoute.js
+++ controllers/contactRoute.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
+const withAuth = require('../middleware/withAuth');
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 
-app.get('/contact', (req,res)=>{
-    res.sendFile(__dirname + '/public/contact')
+app.get('/contact', withAuth, (req,res)=>{
+    res.render('contact', { 
+        //!SOMETHING GOES HERE , 
+        logged_in: req.session.logged_in 
+      });
 })
 
-app.post('/contact', (req,res)=>{
+app.post('/contact', withAuth, (req,res)=>{
 
     // defining our transporter which will use gmail while passing our credentials which are located elsewhere in our project
     const transporter = nodemailer.createTransport({
@@ -19,7 +23,7 @@ app.post('/contact', (req,res)=>{
     })
     const mailOptions = {
         from: req.body.email,
-        to: Email,
+        to: process.env.Email,
         subject: `${req.body.email} has sent us a message! ${req.body.subject}`,
         text: req.body.message
     }
