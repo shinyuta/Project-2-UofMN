9e41b238d9c6dacbe175cc46d9e022f60c7e1085
PsychoRSS <Bobgonzalez516@gmail.com>
2023-09-25 19:36:35 -0600

    gonna commit to main branch

--- models/index.js
+++ models/index.js
@@ -1,15 +1,8 @@
 const Cup = require('./Cup');
 const User = require('./User');
-const Launch = require('./launches');
+const Launch = require('./Launch');
 const UserCup = require('./UserCup');
 
-// Cup.belongsTo(User,{
-//     foreignKey: 'user_id',
-// })
-
-// User.hasMany(Cup, {
-//     foreignKey: 'user_id'
-// })
 
 Launch.hasMany(Cup, {
   foreignKey: 'launch_id'
@@ -20,8 +13,8 @@ Cup.hasOne(Launch,{
 })
 
 User.belongsToMany(Cup, { through: {model:UserCup} });
+
+
 Cup.belongsToMany(User, { through: {model:UserCup} });
-// Cup.hasMany(User, { through: 'UserCup'});
-// User.hasMany(Cup, { through: 'UserCup'});
 
 module.exports = {Cup, User, Launch, UserCup};
