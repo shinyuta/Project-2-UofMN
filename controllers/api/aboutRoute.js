070774538d689223c54daae18ccfe84ac12ce685
Gabe <gabriel.velasquez0116@gmail.com>
2023-09-28 14:24:32 -0700

    made edits to routes and added about route, just need to build it

---
+++ controllers/api/aboutRoute.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+router.get('/about', (req, res) => {
+    res.render('about', {
+        layout: 'main',
+        // logged_in: req.session.logged_in 
+    });
+});
