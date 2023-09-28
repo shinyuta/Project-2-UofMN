b3a677217864f7f95806319337efbf7e41cbb790
Gabe <gabriel.velasquez0116@gmail.com>
2023-09-28 15:01:42 -0700

    put signup route and linked via handlebars

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const emailRoute = require('./emailRoute');
 const userRoutes = require('./userRoute');
+const aboutRoute = require('./aboutRoute');
+const signupRoute = require('./signupRoute');
 
 router.use('/users', userRoutes);
 router.use('/emailRoute', emailRoute);
-// router.use('/aboutRoute', aboutRoute);
+router.use('/aboutRoute', aboutRoute);
+router.use('/signupRoute', signupRoute);
 
 
 module.exports = router;
--- controllers/api/signupRoute.js
+++ controllers/api/signupRoute.js
@@ -1,4 +1,10 @@
+const router = require('express').Router();
 
-
+router.get('/signup', (req, res) => {
+    res.render('signup', {
+        layout: 'main',
+        // logged_in: req.session.logged_in 
+    });
+});
 
 module.exports = router;
