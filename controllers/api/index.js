66c36618897225587e36d8af0e8ad93dfa0a7345
Gabe <gabriel.velasquez0116@gmail.com>
2023-09-28 15:32:14 -0700

    pushing changes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,11 +3,14 @@ const emailRoute = require('./emailRoute');
 const userRoutes = require('./userRoute');
 const aboutRoute = require('./aboutRoute');
 const signupRoute = require('./signupRoute');
+// const loginRoute = require('./loginRoute');
 
 router.use('/users', userRoutes);
 router.use('/emailRoute', emailRoute);
 router.use('/aboutRoute', aboutRoute);
 router.use('/signupRoute', signupRoute);
+router.use('./userRoute', userRoutes);
+// router.use('./loginRoute', loginRoutes);
 
 
 module.exports = router;
