9f4e2a20a4b6bc788e769bca30ab1bc080545311
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-10-01 10:26:33 -0500

    grabbed connection from our repo, i had acciently merged it with what i had been trying

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,7 @@
 const Sequelize = require('sequelize');
-const express = require("express");
 require('dotenv').config();
 
 let sequelize;
-const app = express();
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
@@ -18,9 +11,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
-      port: PORT
+      port: 3306
     }
   );
 }
