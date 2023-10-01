405248c0b1ddeb7ffbaab0ee1c68d43679f3e885
astrunk87 <134554632+astrunk87@users.noreply.github.com>
2023-10-01 10:36:18 -0500

    changing host

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: '127.0.0.1',
+      host: 'local host',
       dialect: 'mysql',
       port: 3306
     }
